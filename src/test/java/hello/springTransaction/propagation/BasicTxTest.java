7d480c4b518951635a4b16625e97115b16fe729d
dugksl23 <dugksl23@gmail.com>
2024-05-15 16:26:12 +0900

    트랜잭션 독립된 환경에서 두 번 사용 에제 테스트

--- src/test/java/hello/springTransaction/propagation/BasicTxTest.java
+++ src/test/java/hello/springTransaction/propagation/BasicTxTest.java
@@ -1,13 +1,17 @@
 package hello.springTransaction.propagation;
 
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -16,6 +20,7 @@ import javax.sql.DataSource;
 
 @SpringBootTest
 @Slf4j
+@Execution(ExecutionMode.CONCURRENT)
 public class BasicTxTest {
 
     @Autowired
@@ -33,19 +38,39 @@ public class BasicTxTest {
 
 
     @Test
-    void commit(){
+    void commitV1(){
 
-        log.info("Transaction 시작");
-        TransactionStatus transaction = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("TransactionV1 시작");
+        TransactionStatus transaction1 = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
-        log.info("Transaction Commit 코드 실행");
-        transactionManager.commit(transaction);
-        log.info("Transaction Commit 완료");
+        log.info("TransactionV1-conn00 Commit 코드 실행");
+        transactionManager.commit(transaction1);
+        log.info("TransactionV1-conn00 Commit 완료");
+
+    }
+
+    @Test
+    void commitV2(){
+
+        log.info("Transaction V2 시작");
+        log.info("Transaction V2 conn 1 시작");
+        TransactionStatus transaction1 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("conn 1 commit 시작");
+        transactionManager.commit(transaction1);
+        log.info("conn 1 Commit 완료");
+
+        log.info("TransactionV2 conn02 시작");
+        TransactionStatus transaction2 = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("conn02 rollback 시작");
+        transactionManager.rollback(transaction2);
+        log.info("conn02 rollback 완료");
 
     }
 
 
-    @Test
+//    @Test
     void rollback(){
 
         log.info("Transaction 시작");
@@ -56,4 +81,5 @@ public class BasicTxTest {
         log.info("Transaction rollback 완료");
 
     }
+
 }
